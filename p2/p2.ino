4b05350c73dcdf0eed9e6dc960d59e16a0879b88
fabrizio morbini <fmorbini@gmail.com>
2021-06-26 21:44:29 -0700

    Update p2.ino
    complete code

--- p2/p2.ino
+++ p2/p2.ino
@@ -12,6 +12,7 @@
 
 Servo myservo;  // create servo object to control a servo
 Servo myservo2;  // create servo object to control a servo
+Servo myservo3;  // create servo object to control a servo
 
 int potpin = 0;  // analog pin used to connect the potentiometer
 int val;    // variable to read the value from the analog pin
@@ -32,9 +33,19 @@ float deltax = 1;
 float xf = .5;
 float yf = .5;
 
+void liftPen();
+void moveTo(float offsetx, float xf, float yf, float x, float y);
+
 void setup() {
   myservo.attach(2);  // attaches the servo on pin 9 to the servo object
-  myservo2.attach(4);  // attaches the servo on pin 9 to the servo object
+  myservo2.attach(3);  // attaches the servo on pin 9 to the servo object
+  myservo3.attach(4);  // attaches the servo on pin 9 to the servo object
+  pinMode(22, OUTPUT);
+  pinMode(24, OUTPUT);
+  digitalWrite(22, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(24, LOW);   // turn the LED on (HIGH is the voltage level)
+  liftPen();
+  moveTo(deltax, xf, yf, 0, 0);
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
   // prints text and menu options for user to select from
@@ -48,24 +59,66 @@ void setup() {
   Serial.println(F("4. print angle"));
   Serial.println(F("5. go"));
   Serial.println(F("6. clock"));
-
+  Serial.println(F("7. away"));
+  Serial.println(F("8. near"));
+  goAway();
 }
 
 float l1 = 3;
 float l2 = 3;
 float l1s = pow(l1, 2);
 float l2s = pow(l2, 2);
-int clock[4] = {1, 2, 3, 4};
+int clock[4] = {2, 1, 2, 9};
+
+void incrementTimeByOneMinute() {
+  if (clock[3] == 9) {
+    clock[3] = 0;
+    if (clock[2] == 5) {
+      clock[2] = 0;
+      if (clock[0] < 2 && clock[1] == 9) {
+        clock[1] = 0;
+        clock[0]++;
+      } else if (clock[0] == 2 && clock[1] == 3) {
+        clock[0] = 0;
+        clock[1] = 0;
+      } else {
+        clock[1]++;
+      }
+    } else {
+      clock[2]++;
+    }
+  } else {
+    clock[3]++;
+  }
+}
+
+void goAway() {
+  digitalWrite(24, LOW);
+  delay(100);
+  digitalWrite(22, HIGH);
+  delay(15000);
+  digitalWrite(22, LOW);
+}
+
+void comeNear() {
+  digitalWrite(22, LOW);
+  delay(100);
+  digitalWrite(24, HIGH);
+  delay(15000);
+  digitalWrite(24, LOW);
+}
 
 void liftPen() {
-
+  myservo3.write(0);              // sets the servo position according to the scaled value
 }
 
 void lowerPen() {
-
+  myservo3.write(40);              // sets the servo position according to the scaled value
 }
 
 void printClock() {
+  comeNear();
+  goAway();
   for (int i = 0; i < 4; i++) {
     Serial.println("digit");
     int d = clock[i];
@@ -114,19 +167,24 @@ void printClock() {
     }
     Serial.println(l);
     liftPen();
+    delay(500);
+    float ayf = yf * (1 + 0.1 * i);
     for (int j = 0; j < l; j++) {
       Serial.print("  segment ");
       Serial.println(j);
       if (j == 0) {
-        moveTo(deltax * (i + 1), xf, yf, n[j][0], n[j][1]);
+        moveTo(deltax + (i + 1), xf, ayf, n[j][0], n[j][1]);
+        delay(500);
         lowerPen();
+        delay(500);
       } else {
-        plotSegment(30, deltax * (i + 1), xf, yf, n[j - 1][0], n[j - 1][1], n[j][0], n[j][1]);
+        plotSegment(30, deltax + (i + 1), xf, ayf, n[j - 1][0], n[j - 1][1], n[j][0], n[j][1]);
       }
     }
     Serial.println("done digit");
   }
   liftPen();
+  delay(500);
   moveTo(deltax, xf, yf, 0, 0);
 }
 
@@ -141,9 +199,9 @@ void convertToAngle(float x, float y, float & a1, float & a2) {
 }
 
 void set(float a1, float a2) {
-  myservo.write(180 - a1);              // sets the servo position according to the scaled value
+  myservo.write(a1);              // sets the servo position according to the scaled value
   myservo2.write(180 - a2);                // sets the servo position according to the scaled value
-  //delay(1);
+  delay(20);
 }
 
 void moveTo(float offsetx, float xf, float yf, float x, float y) {
@@ -168,11 +226,25 @@ void plotSegment(int steps, float offsetx, float xf, float yf, float x1, float y
 
 float gx = 0;
 float gy = 0;
+unsigned long lastIncrement = millis();
+unsigned const long DEFAULT_PERIOD = 60000;
+unsigned long period = DEFAULT_PERIOD;
+long adjust = 0;
+printClock();
 void loop()
 {
-  if (Serial.available())  //Constantly checks to see if anything has been sent over the USB Connection and if it needs to be processed
+  unsigned long delta = millis() - lastIncrement;
+  if (delta > period) {
+    lastIncrement = millis();
+    adjust -= (delta - DEFAULT_PERIOD);
+    period = DEFAULT_PERIOD + adjust;
+    incrementTimeByOneMinute();
+    printClock();
+  }
+  /*
+    if (Serial.available())  //Constantly checks to see if anything has been sent over the USB Connection and if it needs to be processed
 
-  {
+    {
     char val = Serial.read(); //Reads a single letter
     if (val == '1') {
       while (!Serial.available()) {}
@@ -202,5 +274,12 @@ void loop()
     if (val == '6') {
       printClock();
     }
-  }
+    if (val == '7') {
+      goAway();
+    }
+    if (val == '8') {
+      comeNear();
+    }
+    }
+  */
 }
